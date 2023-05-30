9cda76da23a197d13388e169dd09c523a0482651
krish-vijayan <krishvijayan55@gmail.com>
2023-05-30 18:41:09 -0400

    Initial Commit

---
+++ HashtagGenerator/HashtagGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashtagGeneratorApp.swift
+//  HashtagGenerator
+//
+//  Created by Krish Vijayan on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct HashtagGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
